feab419db4dd35e6c226c5fe4a817b50a1ef4ba6
NafisaKaruri <nafeesahassan016@gmail.com>
2024-09-30 13:44:59 +0000

    task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,7 +1,7 @@
 export default function taskBlock(trueOrFalse) {
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    var task = true;
+    var task2 = false;
   }
 
   var task = false;
