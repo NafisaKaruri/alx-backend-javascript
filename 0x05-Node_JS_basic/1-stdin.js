480ede88a1a166698ab35d789e97caab42a46bb3
NafisaKaruri <nafeesahassan016@gmail.com>
2025-02-01 10:02:12 +0000

    task1?

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,7 +3,7 @@ process.stdout.write('Welcome to ALX, what is your name?\n');
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
 
-  if (chunk) {
+  if (chunk !== null) {
     process.stdout.write(`Your name is: ${chunk}`);
   }
 });
