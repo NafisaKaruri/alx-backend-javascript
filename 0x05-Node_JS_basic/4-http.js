e002ec7584d472abf419659d02fa5066577b4d0e
NafisaKaruri <nafeesahassan016@gmail.com>
2025-02-02 22:03:46 +0000

    holberton?

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -5,10 +5,12 @@ const HOST = 'localhost';
 const app = http.createServer();
 
 app.on('request', (_, res) => {
+  const responseText = 'Hello Holberton School!';
+
   res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', 'Hello ALX!');
+  res.setHeader('Content-Length', responseText.length);
   res.statusCode = 200;
-  res.write(Buffer.from('Hello ALX!'));
+  res.write(Buffer.from(responseText));
 });
 
 app.listen(PORT, HOST, () => {
