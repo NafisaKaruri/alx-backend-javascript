340984a3335c5f01e48ae320a7feb5da2c8a046b
NafisaKaruri <nafeesahassan016@gmail.com>
2025-02-02 21:51:47 +0000

    task4

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const PORT = 1245;
+const HOST = 'localhost';
+const app = http.createServer();
+
+app.on('request', (_, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', 'Hello ALX!');
+  res.statusCode = 200;
+  res.write(Buffer.from(responseText));
+});
+
+app.listen(PORT, HOST, () => {
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
+});
+
+module.exports = app;
