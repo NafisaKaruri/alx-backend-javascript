14f37ed5c3f7df239c1589b85fc7bc70b9c1d39d
NafisaKaruri <nafeesahassan016@gmail.com>
2025-02-02 21:56:06 +0000

    task4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -8,7 +8,7 @@ app.on('request', (_, res) => {
   res.setHeader('Content-Type', 'text/plain');
   res.setHeader('Content-Length', 'Hello ALX!');
   res.statusCode = 200;
-  res.write(Buffer.from(responseText));
+  res.write(Buffer.from('Hello ALX!'));
 });
 
 app.listen(PORT, HOST, () => {
